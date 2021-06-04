Refactored SSD detector (prior box calculation fixed)

data_handling/barcode_dataset.py
@@ -19,18 +19,7 @@ class BarcodeDataset:
         self.anchorBoxes = None
         self.boundingBoxDict = None
 
-    @staticmethod
-    def cluster_bounding_boxes(bb_coords, cluster_count):
-        kmedoids = KMedoids(n_clusters=cluster_count)
-        widths = bb_coords[:, 2] - bb_coords[:, 0]
-        heights = bb_coords[:, 3] - bb_coords[:, 1]
-        bb_dimensions = np.stack([widths, heights], axis=1)
-        kmedoids.fit(X=bb_dimensions)
-        counter = Counter(kmedoids.labels_)
-        print("counter={0}".format(counter))
-        return kmedoids.cluster_centers_
-
-    def preprocess_dataset(self, barcodes_with_text_detection_path, cluster_count):
+    def preprocess_dataset(self, barcodes_with_text_detection_path):
         all_paths = [os.path.join(barcodes_with_text_detection_path, f)
                      for f in listdir(barcodes_with_text_detection_path)
                      if isfile(join(barcodes_with_text_detection_path, f))]
@@ -58,15 +47,21 @@ class BarcodeDataset:
                 data_dict = pickle.load(f)
             # "bounding_box": np.stack([scaled_top_left, scaled_bottom_right], axis=0)
             bounding_box = data_dict["bounding_box"]
-            bounding_box_normalized = np.clip(
-                np.array(
-                    [bounding_box[0, 1] / image.shape[1],
-                     bounding_box[0, 0] / image.shape[0],
-                     bounding_box[1, 1] / image.shape[1],
-                     bounding_box[1, 0] / image.shape[0]]), 0.0, 1.0)
+            bounding_box = np.array([
+                data_dict["bounding_box"][0, 1],
+                data_dict["bounding_box"][0, 0],
+                data_dict["bounding_box"][1, 1],
+                data_dict["bounding_box"][1, 0]
+            ])
+            # bounding_box_normalized = np.clip(
+            #     np.array(
+            #         [bounding_box[0, 1] / image.shape[1],
+            #          bounding_box[0, 0] / image.shape[0],
+            #          bounding_box[1, 1] / image.shape[1],
+            #          bounding_box[1, 0] / image.shape[0]]), 0.0, 1.0)
             # Utils.show_image_with_normalized_bounding_boxes(image, [bounding_box_normalized], "normalized_bb")
             barcode_image_sizes_dict[file_name] = np.array([image.shape[0], image.shape[1]])
-            barcode_text_bbs_dict[file_name] = bounding_box_normalized
+            barcode_text_bbs_dict[file_name] = bounding_box
         # Analyze image sizes
         image_sizes = np.stack([shp for shp in barcode_image_sizes_dict.values()], axis=0)
         height_width_ratios = np.array([shp[0] / shp[1] for shp in image_sizes])
@@ -78,7 +73,10 @@ class BarcodeDataset:
         print("Max Ratio:{0}".format(np.max(height_width_ratios)))
         # Cluster bounding boxes
         bb_coords = np.stack([bb for bb in barcode_text_bbs_dict.values()], axis=0)
-        self.anchorBoxes = BarcodeDataset.cluster_bounding_boxes(bb_coords=bb_coords, cluster_count=cluster_count)
         self.boundingBoxDict = {}
         for k, v in barcode_text_bbs_dict.items():
             self.boundingBoxDict[os.path.join(barcodes_with_text_detection_path, "{0}.png".format(k))] = [v]
+
+        # self.anchorBoxes = BarcodeDataset.cluster_bounding_boxes(bb_coords=bb_coords, cluster_count=cluster_count)
+        # self.boundingBoxDict = {}
+

entry_points/train_ssd_text_detector.py
@@ -12,10 +12,11 @@ input_dims = (image_height, image_width, 3)
 anchor_box_counts = (2, 6)
 cluster_count = sum(anchor_box_counts)
 batch_size = 64
-epoch_count = 1000
+epoch_count = 500
+model_name = "barcode_detector_2"
 
 
-def data_processing_for_text_detection():
+def train_blaze_ssd_detector():
     gpus = tf.config.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
@@ -25,15 +26,13 @@ def data_processing_for_text_detection():
     model_path = os.path.join(file_path, "..", "saved_models")
 
     barcode_dataset = BarcodeDataset(dataset_path=barcode_folder_path)
-    barcode_dataset.preprocess_dataset(
-        barcodes_with_text_detection_path=barcodes_with_text_detection_path, cluster_count=cluster_count)
+    barcode_dataset.preprocess_dataset(barcodes_with_text_detection_path=barcodes_with_text_detection_path)
 
     blaze_ssd_detector = BlazeSsdDetector(
-        model_name="barcode_detector",
+        model_name=model_name,
         model_path=model_path,
-        input_shape=input_dims,
-        prior_boxes=barcode_dataset.anchorBoxes
+        input_shape=input_dims
     )
-    blaze_ssd_detector.build_detector()
+    blaze_ssd_detector.build_detector(bounding_box_dict=barcode_dataset.boundingBoxDict)
     blaze_ssd_detector.train(batch_size=batch_size, epoch_count=epoch_count,
                              training_set=barcode_dataset.boundingBoxDict)

text_detection/align_bb_data.py
@@ -5,13 +5,21 @@ from utils import Utils
 
 
 class AlignBBData(tf.keras.layers.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, prior_box_tensors, **kwargs):
         super().__init__(**kwargs)
+        for prior_id, prior_tensor in enumerate(prior_box_tensors):
+            self.add_weight("prior_{0}".format(prior_id), shape=prior_tensor.shape,
+                            initializer=tf.keras.initializers.Constant(value=0),
+                            dtype=prior_tensor.dtype, trainable=False)
+            self.weights[-1].assign(prior_tensor)
+        assert len(self.weights) == len(prior_box_tensors)
+        assert all(["prior_{0}".format(prior_id) in self.weights[prior_id].name
+                    for prior_id in range(len(prior_box_tensors))])
 
     def call(self, inputs, training=None):
         confidence_outputs = inputs[0]
         regression_outputs = inputs[1]
-        prior_boxes = inputs[2]
+        prior_boxes = self.weights
         # assert confidence_outputs.shape[0] == regression_outputs.shape[0]
         # batch_size = confidence_outputs[0].shape[0]
         batch_size = tf.shape(confidence_outputs[0])[0]